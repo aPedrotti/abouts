dcdd2fe2154c9327b3bb757b51c367d94a2b8e5d
pedrotti <andre.oliveira3@gerdau.com.br>
2023-10-04 00:10:50 -0300

    migrate variables

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_version" {
+  default = "ami-067d1e60475437da2"
+  description = "Image ID from AMI catalog regarding this region"
+}
+
+variable "ssh_public_key" {
+  type = "string"
+  description = "Your public key will be attached to the ec2"
+}
