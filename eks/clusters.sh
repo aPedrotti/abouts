b1f90a3f292b24f41b03f54f9dc744c9cc532cd2
pedrotti <andre.oliveira3@gerdau.com.br>
2023-10-27 22:48:58 -0300

    about eksctl

---
+++ eks/clusters.sh
@@ -0,0 +1,24 @@
+
+
+create(){
+  eksctl create cluster \
+ --name platform \
+ --nodegroup-name devops \
+ --node-type t3.medium \
+ --nodes 1 \
+ --nodes-min 1 \
+ --nodes-max 2 \
+ --managed \
+ --version 1.26 \
+ --region us-east-1
+}
+update(){
+  eksctl utils update-cluster-logging --enable-types=all --region=us-east-1 --cluster=platform
+}
+
+
+delete(){
+eksctl delete cluster \
+ --name platform \
+ --region=us-east-1
+}
